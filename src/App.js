59209fa425a8a1ff9372465803f52d5410fdb7f9
Antonio Tobar <antoniotobar@Antonios-MacBook-Air.local>
2019-09-03 15:16:58 -0500

    stepper counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      counter: 0,
+    }
+
+  }
+  incrementCounter = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+  }
+
+  decreaseCounter = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+  }
+
+  render() {
+
+    return(
+      <div className="counter">
+        <button className="minus" onClick={this.decreaseCounter}> - </button>
+        <h1>{this.state.counter}</h1>
+        <button className="plus" onClick={this.incrementCounter}> + </button>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
